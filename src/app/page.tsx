65d36f767b298a8397be0a5f57b67612ba216971
Samarth Kamble <kamblesamarth04@gmail.com>
2024-11-26 22:17:26 +0530

    Implement user authentication features with current user query and user button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { useAuthActions } from "@convex-dev/auth/react";
+import UserButton from "./features/auth/_components/UserButton";
 
 const Home = () => {
-  return <div>Logged In !</div>;
+  return (
+    <div>
+      Logged In !
+      <UserButton />
+    </div>
+  );
 };
 
 export default Home;
