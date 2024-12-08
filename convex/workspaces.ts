3f34f27ddc0d505c8731467c4c6961d63a181353
Samarth Kamble <kamblesamarth04@gmail.com>
2024-12-08 20:05:22 +0530

    Implement functionality to generate a new join code for workspaces

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -5,13 +5,44 @@ import { auth } from "./auth";
 
 const generateCode = () => {
   const code = Array.from(
-      { length: 6 },
-      () =>
-          "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
+    { length: 6 },
+    () => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
   ).join("");
 
   return code;
 };
+
+export const newJionCode = mutation({
+  args: {
+    workspaceId: v.id("workspaces"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx);
+
+    if (!userId) {
+      throw new Error("User not found");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member || member.role !== "admin") {
+      throw new Error("Unauthorized");
+    }
+
+    const jionCode = generateCode();
+
+    await ctx.db.patch(args.workspaceId, {
+      jionCode,
+    });
+
+    return args.workspaceId;
+  },
+});
 
 export const create = mutation({
   args: {
@@ -31,16 +62,16 @@ export const create = mutation({
       jionCode,
     });
 
-    await ctx.db.insert("members",{
+    await ctx.db.insert("members", {
       userId,
       workspaceId,
-      role: "admin"
-    })
+      role: "admin",
+    });
 
-    await ctx.db.insert("channels",{
-      name:"general",
-      workspaceId
-    })
+    await ctx.db.insert("channels", {
+      name: "general",
+      workspaceId,
+    });
 
     return workspaceId;
   },
@@ -50,13 +81,13 @@ export const get = query({
   args: {},
   handler: async (ctx) => {
     const userId = await auth.getUserId(ctx);
-    if(!userId){
-      return []
+    if (!userId) {
+      return [];
     }
     const members = await ctx.db
-        .query("members")
-        .withIndex("by_user_id", (q) => q.eq("userId", userId))
-        .collect();
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
 
     const workspaceIds = members.map((member) => member.workspaceId);
 
@@ -84,47 +115,47 @@ export const getById = query({
       throw new Error("User not found");
     }
     const member = await ctx.db
-        .query("members")
-        .withIndex("by_workspace_id_user_id", (q)=>
-        q.eq("workspaceId",args.id).eq("userId",userId),
-        )
-        .unique()
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
 
-      if (!member){
-        return null
-      }
+    if (!member) {
+      return null;
+    }
     return await ctx.db.get(args.id);
   },
 });
 
 export const update = mutation({
-  args:{
+  args: {
     id: v.id("workspaces"),
     name: v.string(),
   },
-  handler: async(ctx,args) => {
+  handler: async (ctx, args) => {
     const userId = await auth.getUserId(ctx);
     if (!userId) {
       throw new Error("User not found");
     }
     const member = await ctx.db
-        .query("members")
-        .withIndex("by_workspace_id_user_id", (q)=>
-            q.eq("workspaceId",args.id).eq("userId",userId),
-        )
-        .unique()
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
 
-      if(!member || member.role !== "admin"){
-        throw new  Error("Unauthorized");
-      }
+    if (!member || member.role !== "admin") {
+      throw new Error("Unauthorized");
+    }
 
-      await ctx.db.patch(args.id, {
-        name:args.name,
-      })
+    await ctx.db.patch(args.id, {
+      name: args.name,
+    });
 
     return args.id;
-  }
-})
+  },
+});
 
 export const remove = mutation({
   args: {
@@ -134,30 +165,29 @@ export const remove = mutation({
     const userId = await auth.getUserId(ctx);
 
     if (!userId) {
-      throw new Error("Unauthorized")
+      throw new Error("Unauthorized");
     }
 
     const member = await ctx.db
-        .query("members")
-        .withIndex("by_workspace_id_user_id", (q) =>
-            q.eq("workspaceId", args.id).eq("userId", userId),
-        )
-        .unique();
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
 
     if (!member || member.role !== "admin") {
       throw new Error("Unauthorized");
     }
 
-    const [members, ] = await Promise.all([
+    const [members] = await Promise.all([
       ctx.db
-          .query("members")
-          .withIndex("by_workspace_id", (q) => q.eq("workspaceId", args.id))
-          .collect(),
-
+        .query("members")
+        .withIndex("by_workspace_id", (q) => q.eq("workspaceId", args.id))
+        .collect(),
     ]);
 
     for (const member of members) {
-      await ctx.db.delete(member._id)
+      await ctx.db.delete(member._id);
     }
 
     await ctx.db.delete(args.id);
--- src/app/workspace/[workspaceId]/_components/InviteModal.tsx
+++ src/app/workspace/[workspaceId]/_components/InviteModal.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { toast } from "sonner";
+import { CopyIcon, RefreshCcw } from "lucide-react";
 
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -9,9 +12,9 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { CopyIcon } from "lucide-react";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { toast } from "sonner";
+import { useNewJoinCode } from "@/features/workspaces/api/use-new-jion-code";
+import { useConfirm } from "@/hooks/use-confirm";
 
 interface InviteModalProps {
   open: boolean;
@@ -22,6 +25,29 @@ interface InviteModalProps {
 
 const InviteModal = ({ open, setOpen, name, jionCode }: InviteModalProps) => {
   const workspaceId = useWorkspaceId();
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Are youb Sure?",
+    "This will generate a new code and invalidate the old one and you will have to share the new code with your team again."
+  );
+  const { mutate, isPending } = useNewJoinCode();
+
+  const handleNewCode = async () => {
+    const ok = await confirm();
+
+    if (!ok) return;
+
+    mutate(
+      { workspaceId },
+      {
+        onSuccess: () => {
+          toast.success("New Code Generated");
+        },
+        onError: () => {
+          toast.error("Failed to enerate new code");
+        },
+      }
+    );
+  };
 
   const handleCopy = () => {
     const inviteLink = `${window.location.origin}/join/${workspaceId}`;
@@ -30,25 +56,41 @@ const InviteModal = ({ open, setOpen, name, jionCode }: InviteModalProps) => {
       .then(() => toast.success("Invaite Link Copied to Clipboard"));
   };
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Invite People to {name}</DialogTitle>
-          <DialogDescription>
-            Share this code with others to invite them to your workspace.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex flex-col gap-y-4 items-center justify-center py-10">
-          <p className="text-4xl font-bold tracking-wider uppercase">
-            {jionCode}
-          </p>
-          <Button variant={"ghost"} size={"sm"} onClick={handleCopy}>
-            Copy Link
-            <CopyIcon className="size-4 ml-2" />
-          </Button>
-        </div>
-      </DialogContent>
-    </Dialog>
+    <>
+      <ConfirmDialog />
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Invite People to {name}</DialogTitle>
+            <DialogDescription>
+              Share this code with others to invite them to your workspace.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col gap-y-4 items-center justify-center py-10">
+            <p className="text-4xl font-bold tracking-wider uppercase">
+              {jionCode}
+            </p>
+            <Button variant={"ghost"} size={"sm"} onClick={handleCopy}>
+              Copy Link
+              <CopyIcon className="size-4 ml-2" />
+            </Button>
+          </div>
+          <div className="flex items-center justify-between w-full">
+            <Button
+              disabled={isPending}
+              onClick={handleNewCode}
+              variant={"outline"}
+            >
+              New Code
+              <RefreshCcw className="size-4 ml-2" />
+            </Button>
+            <DialogClose asChild>
+              <Button>Close</Button>
+            </DialogClose>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
