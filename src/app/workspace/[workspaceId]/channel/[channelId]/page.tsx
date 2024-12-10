5da1a83009fb4b1101ba1cf92d04d577a807f8aa
Samarth Kamble <kamblesamarth04@gmail.com>
2024-12-10 21:01:22 +0530

    Implement the channel removal and update functionalities with hook and channel header

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,5 +1,41 @@
+"use client";
+
+import { Loader, TriangleAlert } from "lucide-react";
+
+import { useGetChannel } from "@/features/channels/api/use-get-channel";
+
+import { useChannelId } from "@/hooks/use-channel-id";
+import ChannelHeader from "./_components/ChannelHeader";
+
 const ChannelIdPage = () => {
-  return <div>Channel Id Page</div>;
+  const channelId = useChannelId();
+
+  const { data: channel, isLoading: channelLoading } = useGetChannel({
+    id: channelId,
+  });
+
+  if (channelLoading || status === "LoadingFirstPage") {
+    return (
+      <div className="h-full flex-1 flex items-center justify-center">
+        <Loader className="animate-spin size-5 text-muted-foreground" />
+      </div>
+    );
+  }
+
+  if (!channel) {
+    return (
+      <div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center">
+        <TriangleAlert className="size-5 text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">Channel Not Found</span>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col h-full">
+      <ChannelHeader title={channel.name} />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
