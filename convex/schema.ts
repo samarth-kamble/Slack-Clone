f0b8887400d873a4244001fa817aba7f941c921b
Samarth Kamble <kamblesamarth04@gmail.com>
2024-12-11 19:51:15 +0530

    Finilise the schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -9,7 +9,6 @@ const schema = defineSchema({
     userId: v.string(),
     jionCode: v.string(),
   }),
-
   members: defineTable({
     userId: v.id("users"),
     workspaceId: v.id("workspaces"),
@@ -18,12 +17,15 @@ const schema = defineSchema({
     .index("by_user_id", ["userId"])
     .index("by_workspace_id", ["workspaceId"])
     .index("by_workspace_id_user_id", ["workspaceId", "userId"]),
-
   channels: defineTable({
     name: v.string(),
     workspaceId: v.id("workspaces"),
   }).index("by_workspace_id", ["workspaceId"]),
-
+  conversations: defineTable({
+    workspaceId: v.id("workspaces"),
+    memberOneId: v.id("members"),
+    memberTwoId: v.id("members"),
+  }).index("by_workspace_id", ["workspaceId"]),
   messages: defineTable({
     body: v.string(),
     image: v.optional(v.id("_storage")),
@@ -31,9 +33,28 @@ const schema = defineSchema({
     workspaceId: v.id("workspaces"),
     channelId: v.id("channels"),
     parentMessageId: v.optional(v.id("messages")),
-    // TODO: Add a conversationId
+    conversationId: v.optional(v.id("conversations")),
     updatedAt: v.number(),
-  }),
+  })
+    .index("by_workspace_id", ["workspaceId"])
+    .index("by_member_id", ["memberId"])
+    .index("by_channel_id", ["channelId"])
+    .index("by_conversation_id", ["conversationId"])
+    .index("by_parent_message_id", ["parentMessageId"])
+    .index("by_channel_id_parent_message_id_conversation_id", [
+      "channelId",
+      "parentMessageId",
+      "conversationId",
+    ]),
+  reactions: defineTable({
+    workspaceId: v.id("workspaces"),
+    messageId: v.id("messages"),
+    memberId: v.id("members"),
+    value: v.string(),
+  })
+    .index("by_workspace_id", ["workspaceId"])
+    .index("by_message_id", ["messageId"])
+    .index("by_member_id", ["memberId"]),
 });
 
 export default schema;
